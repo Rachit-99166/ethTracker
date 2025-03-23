2b85ae3148883ca1c40e36a660640c2d082361bd
Rachit <rachitdua997@gmail.com>
2025-03-23 12:22:34 +0530

    Update DepositController.java

--- src/main/java/com/demo/ethtracker/controller/DepositController.java
+++ src/main/java/com/demo/ethtracker/controller/DepositController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.math.BigInteger;
 
 import com.demo.ethtracker.entity.Deposit;
