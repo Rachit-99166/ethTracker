5c3e622f52696688f9bc9b4438e5ba8af9d278aa
Rachit <rachitdua997@gmail.com>
2025-03-28 00:41:37 +0530

    Update EthtrackerApplication.java

--- src/main/java/com/demo/ethtracker/EthtrackerApplication.java
+++ src/main/java/com/demo/ethtracker/EthtrackerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EthtrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EthtrackerApplication.class, args);
+        SpringApplication.run(EthtrackerApplication.class, args); 
     }
 
 }
